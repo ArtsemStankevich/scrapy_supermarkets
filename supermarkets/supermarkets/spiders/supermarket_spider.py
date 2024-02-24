c6835a673ed1d6b07e24127a898904ac82ff79d1
ArtsemStankevich <artsemstankevich@gmail.com>
2024-02-24 14:36:50 +0100

    replace validators to another file and update spader

--- supermarkets/supermarkets/spiders/supermarket_spider.py
+++ supermarkets/supermarkets/spiders/supermarket_spider.py
@@ -1,12 +1,36 @@
 import scrapy
 import re
 import json
-from pydantic import BaseModel, ValidationError, validator
+from pydantic import BaseModel, validator
+from ..validators import validate_fields, validate_opening_hours
 
-class StartURLSFromFile:
-    def __init__(self):
-        with open("start_urls.txt", "r") as f:
-            self.start_urls = f.read().split("\n")
+business_data = [
+    {'url_part': 'lidl', 'name': 'Lidl'},
+    {'url_part': 'polomarket', 'name': 'polomarket'},
+    {'url_part': 'biedronka', 'name': 'Biedronka'},
+    {'url_part': 'intermarche-super', 'name': 'Intermarche Super'},
+    {'url_part': 'castorama', 'name': 'Castorama'},
+    {'url_part': 'carrefour', 'name': 'Carrefour'},
+    {'url_part': 'pepco', 'name': 'Pepco'},
+    {'url_part': 'dino', 'name': 'Dino'},
+    {'url_part': 'zabka', 'name': 'Żabka'},
+    {'url_part': 'kaufland', 'name': 'Kaufland'},
+    {'url_part': 'topmarket', 'name': 'Top Market'},
+    {'url_part': 'carrefour-market', 'name': 'Carrefour Market'},
+    {'url_part': 'stokrotka', 'name': 'Stokrotka'},
+    {'url_part': 'eurospar', 'name': 'Eurospar'},
+    {'url_part': 'stokrotka-express', 'name': 'Stokrotka Express'},
+    {'url_part': 'lewiatan', 'name': 'Lewiatan'},
+    {'url_part': 'spar', 'name': 'Spar'},
+    {'url_part': 'stokrotka-market', 'name': 'Stokrotka Market'},
+    {'url_part': 'auchan-supermarket', 'name': 'Auchan Supermarket'},
+    {'url_part': 'carrefour-express', 'name': 'Carrefour Express'},
+    {'url_part': 'auchan', 'name': 'Auchan'},
+    {'url_part': 'netto', 'name': 'Netto'},
+    {'url_part': 'aldi', 'name': 'Aldi'}
+]
+
+
 class BusinessRecord(BaseModel):
     business: str
     opening_hours: str
@@ -14,51 +38,29 @@ class BusinessRecord(BaseModel):
     city: str
     map_link: str
 
-    @validator('business', 'address', 'city', 'map_link')
-    def validate_fields(cls, value):
-        if not value:
-            raise ValueError('Field cannot be empty')
-        return value
+    _validate_fields = validator('business', 'address', 'city', 'map_link')(validate_fields)
+    _validate_opening_hours = validator('opening_hours')(validate_opening_hours)
 
-    @validator('opening_hours')
-    def validate_opening_hours(cls, value):
-        pattern = r'^\d{2}:\d{2}-\d{2}:\d{2}$'
-        if not re.match(pattern, value):
-            raise ValueError('Invalid opening hours format. Use HH:MM-HH:MM')
-        return value
+class StartURLSFromFile:
+    def __init__(self):
+        with open("start_urls.txt", "r") as f:
+            base_urls = f.read().split("\n")
+        self.start_urls = self.generate_urls(base_urls)
+
+    def generate_urls(self, base_urls):
+        generated_urls = []
+        for business in business_data:
+            generated_urls.append(f"https://{business['url_part']}.okazjum.pl/sklepy/gdansk/")
+        return generated_urls
 
 class SupermarketSpider(scrapy.Spider):
     name = 'okazjum'
+
     start_urls = StartURLSFromFile().start_urls
-
-    business_data = [
-        {'url_part': 'lidl', 'name': 'Lidl'},
-        {'url_part': 'polomarket', 'name': 'polomarket'},
-        {'url_part': 'biedronka', 'name': 'Biedronka'},
-        {'url_part': 'intermarche-super', 'name': 'Intermarche Super'},
-        {'url_part': 'castorama', 'name': 'Castorama'},
-        {'url_part': 'carrefour', 'name': 'Carrefour'},
-        {'url_part': 'pepco', 'name': 'Pepco'},
-        {'url_part': 'dino', 'name': 'Dino'},
-        {'url_part': 'zabka', 'name': 'Żabka'},
-        {'url_part': 'kaufland', 'name': 'Kaufland'},
-        {'url_part': 'topmarket', 'name': 'Top Market'},
-        {'url_part': 'carrefour-market', 'name': 'Carrefour Market'},
-        {'url_part': 'stokrotka', 'name': 'Stokrotka'},
-        {'url_part': 'eurospar', 'name': 'Eurospar'},
-        {'url_part': 'stokrotka-express', 'name': 'Stokrotka Express'},
-        {'url_part': 'lewiatan', 'name': 'Lewiatan'},
-        {'url_part': 'spar', 'name': 'Spar'},
-        {'url_part': 'stokrotka-market', 'name': 'Stokrotka Market'},
-        {'url_part': 'auchan-supermarket', 'name': 'Auchan Supermarket'},
-        {'url_part': 'carrefour-express', 'name': 'Carrefour Express'},
-        {'url_part': 'auchan', 'name': 'Auchan'},
-        {'url_part': 'netto', 'name': 'Netto'},
-        {'url_part': 'aldi', 'name': 'Aldi'}
-    ]
-
+    
+    print(start_urls)
     def parse(self, response):
-        for data in self.business_data:
+        for data in business_data:
             url_part = data['url_part']
             last_part_of_url = response.url.split('/')[2]
             last_part_of_url = last_part_of_url.split('.')[0]
